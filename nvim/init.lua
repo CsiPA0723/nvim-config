e9f2bc7b8afd82ac4bf15600248b4de5d5b349aa
CsiPA0723 <csizipeti0723@gmail.com>
2024-03-22 07:47:04 +0100

    Updated init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -106,7 +106,52 @@ require("lazy").setup({
 			},
 		},
 	},
-	{ "lewis6991/gitsigns.nvim" },
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "│" },
+				change = { text = "│" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			attach_to_untracked = false,
+			current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+			yadm = {
+				enable = false,
+			},
+		},
+	},
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{
 		"nvim-lualine/lualine.nvim",
@@ -149,7 +194,7 @@ require("lazy").setup({
 			tabline = {},
 			winbar = {},
 			inactive_winbar = {},
-			extensions = { "lazy", "neo-tree", "aerial" },
+			extensions = { "lazy", "neo-tree", "aerial", "trouble" },
 		},
 	},
 	{ "nvim-lua/plenary.nvim", lazy = true },
@@ -236,6 +281,7 @@ require("lazy").setup({
 				ufo = true,
 				neotree = true,
 				notify = true,
+				lsp_trouble = true,
 				treesitter = true,
 				rainbow_delimiters = true,
 				telescope = { enabled = true },
@@ -347,6 +393,22 @@ require("lazy").setup({
 				},
 			})
 		end,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = true,
 	},
 })
 
