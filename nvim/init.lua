86240d64424096e020d720e358a9c659502e3fc3
CsiPA0723 <csizipeti0723@gmail.com>
2024-03-22 06:46:43 +0100

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,455 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = "," -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.number = true
+vim.opt.relativenumber = true
+
+vim.o.foldcolumn = "1" -- "0" is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+vim.o.termguicolors = true
+vim.o.mouse = nil
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.shiftround = true
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.colorcolumn = "80"
+vim.o.wrap = false
+
+require("lazy").setup({
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+	},
+	{ "folke/neoconf.nvim", cmd = "Neoconf" },
+	{ "folke/neodev.nvim" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"angular",
+				"bash",
+				"c",
+				"cmake",
+				"comment",
+				"cpp",
+				"css",
+				"csv",
+				"diff",
+				"dockerfile",
+				"dot",
+				"git_config",
+				"git_rebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
+				"haskell",
+				"hlsl",
+				"html",
+				"ini",
+				"javasript",
+				"jsdoc",
+				"json",
+				"json5",
+				"jsonc",
+				"lua",
+				"markdown",
+				"passwd",
+				"php",
+				"phpdoc",
+				"properties",
+				"python",
+				"regex",
+				"scss",
+				"sql",
+				"ssh_config",
+				"tmux",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"query",
+				"xml",
+				"yaml",
+			},
+			sync_install = false,
+			auto_install = true,
+			ignore_install = { "java" },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		},
+	},
+	{ "lewis6991/gitsigns.nvim" },
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "catppuccin",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = { "lazy", "neo-tree", "aerial" },
+		},
+	},
+	{ "nvim-lua/plenary.nvim", lazy = true },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+			"3rd/image.nvim",
+		},
+	},
+	{ "nvim-telescope/telescope.nvim" },
+	{
+		"cuducos/yaml.nvim",
+		ft = { "yaml" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{ "sindrets/diffview.nvim" },
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"rcarriga/nvim-notify",
+		config = true,
+	},
+	{
+		"stevearc/conform.nvim",
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>F",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { { "prettierd", "prettier" } },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		},
+	},
+	{ "mrjones2014/smart-splits.nvim", config = true },
+	{
+		"stevearc/aerial.nvim",
+		opts = { backends = { "treesitter" } },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{ "HiPhish/rainbow-delimiters.nvim" },
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl" },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			integrations = {
+				aerial = true,
+				gitsigns = true,
+				indent_blankline = {
+					enabled = true,
+					scope_color = "lavender",
+					colored_indent_levels = true,
+				},
+				ufo = true,
+				neotree = true,
+				notify = true,
+				treesitter = true,
+				rainbow_delimiters = true,
+				telescope = { enabled = true },
+				which_key = true,
+			},
+		},
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			{
+				"luukvbaal/statuscol.nvim",
+				config = function()
+					local builtin = require("statuscol.builtin")
+					require("statuscol").setup({
+						relculright = true,
+						segments = {
+							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+							{ text = { "%s" }, click = "v:lua.ScSa" },
+							{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+						},
+					})
+				end,
+			},
+		},
+		event = "VeryLazy",
+		opts = {
+			-- INFO: Uncomment to use treeitter as fold provider, otherwise nvim lsp is used
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "treesitter", "indent" }
+			end,
+			open_fold_hl_timeout = 400,
+			close_fold_kinds_for_ft = { "imports", "comment" },
+			preview = {
+				win_config = {
+					border = { "", "─", "", "", "", "─", "", "" },
+					-- winhighlight = "Normal:Folded",
+					winblend = 0,
+				},
+				mappings = {
+					scrollU = "<C-u>",
+					scrollD = "<C-d>",
+					jumpTop = "[",
+					jumpBot = "]",
+				},
+			},
+		},
+		init = function()
+			vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+			vim.o.foldcolumn = "1" -- "0" is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+		end,
+		config = function(_, opts)
+			local handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local totalLines = vim.api.nvim_buf_line_count(0)
+				local foldedLines = endLnum - lnum
+				local suffix = ("  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				local rAlignAppndx = math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+				suffix = (" "):rep(rAlignAppndx) .. suffix
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
+			end
+			opts["fold_virt_text_handler"] = handler
+			require("ufo").setup(opts)
+
+			local wk = require("which-key")
+			wk.register({
+				R = { require("ufo").openAllFolds, "Open All Folds" },
+				M = { require("ufo").closeAllFolds, "Close All Folds" },
+				r = { require("ufo").openFoldsExceptKinds, "Open Folds Except Kinds" },
+				m = { require("ufo").closeFoldsWith, "Close Folds With" },
+			}, { prefix = "<z>" })
+			wk.register({
+				K = {
+					function()
+						local winid = require("ufo").peekFoldedLinesUnderCursor()
+						if not winid then
+							-- choose one of coc.nvim and nvim lsp
+							vim.fn.CocActionAsync("definitionHover") -- coc.nvim
+							vim.lsp.buf.hover()
+						end
+					end,
+					"Peel Folded Lines Under Cursor",
+				},
+			})
+		end,
+	},
+})
+
+vim.notify = require("notify")
+
+vim.cmd.colorscheme("catppuccin")
+
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+local rainbow_delimiters = require("rainbow-delimiters")
+
+vim.g.rainbow_delimiters = {
+	strategy = {
+		[""] = rainbow_delimiters.strategy["global"],
+		vim = rainbow_delimiters.strategy["local"],
+	},
+	query = {
+		[""] = "rainbow-delimiters",
+		lua = "rainbow-blocks",
+	},
+	priority = {
+		[""] = 110,
+		lua = 210,
+	},
+	highlight = highlight,
+}
+require("ibl").setup({ scope = { highlight = highlight } })
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+local wk = require("which-key")
+
+-- recommended mappings
+-- resizing splits
+-- these keymaps will also accept a range,
+-- for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+-- moving between splits
+
+local ss = require("smart-splits")
+
+wk.register({
+	["<A-h>"] = { ss.resize_left, "Resize Left" },
+	["<A-j>"] = { ss.resize_down, "Resize Down" },
+	["<A-k>"] = { ss.resize_up, "Resize Up" },
+	["<A-l>"] = { ss.resize_right, "Resize Right" },
+	["<C-h>"] = { ss.move_cursor_left, "Move to Left Plane" },
+	["<C-j>"] = { ss.move_cursor_down, "Move to Down Plane" },
+	["<C-k>"] = { ss.move_cursor_up, "Move to Up Plane" },
+	["<C-l>"] = { ss.move_cursor_right, "Move to Rigth Plane" },
+	["<C-bs>"] = { ss.move_cursor_previous, "Move to Previous Plane" },
+}, { noremap = true })
+
+wk.register({
+	f = {
+		name = "+file",
+		f = { "<cmd>Telescope find_files<cr>", "Find File" },
+		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+		n = { "<cmd>enew<cr>", "New File" },
+	},
+	-- swapping buffers between windows
+	["<leader>"] = {
+		name = "+swap",
+		h = { ss.swap_buf_left, "Swap to Left Buffer" },
+		j = { ss.swap_buf_down, "Swap to Down Buffer" },
+		k = { ss.swap_buf_up, "Swap to Up Buffer" },
+		l = { ss.swap_buf_right, "Swap to Right Buffer" },
+	},
+	a = { "<cmd>AerialToggle<cr>", "Toggle Aerial" },
+	l = { "<cmd>Lazy<cr>", "Open Lazy Plugin Manager" },
+	n = {
+		name = "Neo-Tree",
+		n = { "<cmd>Neotree toggle position=float<cr>", "Toggle Neo-Tree" },
+		l = { "<cmd>Neotree toggle position=left<cr>", "Open Left" },
+		c = { "<cmd>Neotree current<cr>", "Open Current" },
+	},
+}, { prefix = "<leader>" })
+
+wk.register({
+	["<Space>"] = { ":", "Open Command Mode" },
+}, { mode = "", silent = false })
+
+wk.register({
+	y = { '"+y', "Copy to clipboard" },
+	p = { '"+p', "Paste from clipboard" },
+	P = { '"+P', "Paste from clipboard" },
+}, { mode = { "n", "v" }, noremap = true, prefix = "<leader>" })
