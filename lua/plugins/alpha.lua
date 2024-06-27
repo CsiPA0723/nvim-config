f332c007586a2eb44975877e945e584246dd1cdf
Péter Csizmadia <csizipeti0723@gmail.com>
2024-06-27 22:15:10 +0200

    Remade Alpha dashboard config

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,43 +1,71 @@
-return { -- TODO: Redo dashboard
+--- @param sc string
+--- @param txt string
+--- @param command string|function
+local function button(sc, txt, command)
+	local opts = {
+		position = 'center',
+		shortcut = '[' .. sc .. '] ',
+		cursor = 47,
+		width = 50,
+		keymap = {
+			'n',
+			sc,
+			command,
+			{ noremap = true, silent = true, nowait = true },
+		},
+		align_shortcut = 'right',
+		hl_shortcut = {
+			{ 'Operator', 0, 1 },
+			{ 'Number', 1, #sc + 1 },
+			{ 'Operator', #sc + 1, #sc + 2 },
+		},
+		shrink_margin = false,
+	}
+
+	local function on_press()
+		if type(command) == 'string' then
+			local key =
+				vim.api.nvim_replace_termcodes(command .. '<Ignore>', true, false, true)
+			vim.api.nvim_feedkeys(key, 't', false)
+		elseif type(command) == 'function' then
+			command()
+		else
+			error('Command argument must be a string or a function!', 2)
+		end
+	end
+
+	return {
+		type = 'button',
+		val = txt,
+		on_press = on_press,
+		opts = opts,
+	}
+end
+
+return {
 	{
 		'goolord/alpha-nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
 			local alpha = require('alpha')
-			local startify = require('alpha.themes.startify')
 			local resession = require('resession')
-			local mru = startify.mru
-			local button = startify.button
 
-			local last_session = nil
-
-			local file = io.open(vim.fn.stdpath('data') .. '/last.json', 'r')
-			if file ~= nil then
-				local str = file:read('*a')
-				local json = vim.json.decode(str)
-				last_session = json.lastSession
-				file:close()
-			end
-
-			vim.api.nvim_create_user_command('ReSessionLoad', function(o)
-				resession.load(o.args)
-			end, { nargs = 1 })
-
-			vim.api.nvim_create_user_command('RestoreLastReSession', function()
-				resession.load('last', { attach = false })
-			end, { nargs = 0 })
-
-			local function mru_cwd_title()
-				return 'MRU ' .. vim.fn.getcwd()
-			end
-
-			local my_header = {
+			local header_neovim = {
 				[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
 				[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
 				[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
 				[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
 				[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
 				[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+			}
+
+			local header_neovide = {
+				[[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗██████╗ ███████╗ ]],
+				[[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║██╔══██╗██╔════╝ ]],
+				[[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██║  ██║█████╗   ]],
+				[[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║  ██║██╔══╝   ]],
+				[[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██████╔╝███████╗ ]],
+				[[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═════╝ ╚══════╝ ]],
 			}
 
 			local theme = {
@@ -47,138 +75,66 @@ return { -- TODO: Redo dashboard
 						{ type = 'padding', val = 2 },
 						{
 							type = 'text',
-							val = my_header,
+							val = vim.g.neovide and header_neovide or header_neovim,
 							opts = { position = 'center', hl = 'Type' },
 						},
 					},
 				},
-				top_buttons = {
+				buttons = {
 					type = 'group',
 					val = {
-						{ type = 'padding', val = 2 },
-						button('e', 'New file', '<cmd>ene <BAR> startinsert <CR>'),
+						button('e', '  New file', '<cmd>ene <BAR> startinsert <CR>'),
 						button(
-							's',
-							string.format('Last Session (%s)', last_session or 'None'),
-							last_session ~= nil
-									and string.format('<cmd>ReSessionLoad %s<CR>', last_session)
-								or nil
+							'f',
+							'󰈞  Find file',
+							require('telescope.builtin').find_files
 						),
-					},
-				},
-				managers = {
-					type = 'group',
-					opts = { priority = 1 },
-					val = {
-						{ type = 'padding', val = 1 },
-						{
-							type = 'text',
-							val = 'Managers',
-							opts = { hl = 'SpecialComment' },
-						},
-						{ type = 'padding', val = 1 },
-						button('l', 'Lazy Plugin Manager', '<cmd>Lazy<CR>'),
-						button('m', 'Mason LSP Manager', '<cmd>Mason<CR>'),
-					},
-				},
-				sessions = {
-					type = 'group',
-					val = {
-						{ type = 'padding', val = 1 },
-						{
-							type = 'text',
-							val = 'Sessions',
-							opts = { hl = 'SpecialComment' },
-						},
-						{ type = 'padding', val = 1 },
-						{
-							type = 'group',
-							val = function()
-								local list = {
-									table.unpack(
-										vim.tbl_filter(function(str)
-											return str ~= 'last'
-										end, resession.list()),
-										1,
-										5
-									),
-								}
-								local sessions = {}
-
-								for i, value in ipairs(list) do
-									sessions[i] = button(
-										tostring(i - 1),
-										value,
-										string.format('<cmd>ReSessionLoad %s<CR>', tostring(value))
-									)
-								end
-
-								return sessions
-							end,
-						},
-					},
-				},
-				mru_cwd = {
-					type = 'group',
-					val = {
-						{ type = 'padding', val = 1 },
-						{
-							type = 'text',
-							val = mru_cwd_title,
-							opts = { hl = 'SpecialComment', shrink_margin = false },
-						},
-						{ type = 'padding', val = 1 },
-						{
-							type = 'group',
-							val = function()
-								return { mru(5, vim.fn.getcwd(), 5) }
-							end,
-							opts = { shrink_margin = false },
-						},
-					},
-				},
-				mru = {
-					type = 'group',
-					val = {
-						{ type = 'padding', val = 1 },
-						{ type = 'text', val = 'MRU', opts = { hl = 'SpecialComment' } },
-						{ type = 'padding', val = 1 },
-						{
-							type = 'group',
-							val = function()
-								return { mru(10, nil, 5) }
-							end,
-						},
-					},
-				},
-				bottom_buttons = {
-					type = 'group',
-					val = {
-						{ type = 'padding', val = 1 },
+						button('s', '  Sessions', resession.load),
 						button(
-							'r',
-							'Restore Last Session',
-							'<cmd>RestoreLastReSession<CR>'
+							'o',
+							'  Recently opened files',
+							require('telescope.builtin').oldfiles
 						),
-						button('q', 'Quit', ':qa<CR>'),
+						button(
+							'b',
+							'󰉋  Browse Files',
+							require('telescope').extensions.file_browser.file_browser
+						),
+						button(
+							'g',
+							'󰊄  Live Grep',
+							require('telescope.builtin').live_grep
+						),
+						button(
+							'z',
+							'󰞁  Jump to Zoxide',
+							require('telescope').extensions.zoxide.list
+						),
+						button('c', '  Configuration', function()
+							require('telescope.builtin').find_files({
+								cwd = vim.fn.stdpath('config'),
+							})
+						end),
+						button('l', '󰒲  Lazy Plugin Manager', '<cmd>Lazy<CR>'),
+						button('m', '󰒍  Mason LSP Manager', '<cmd>Mason<CR>'),
+						button('r', '  Restore Last Session', function()
+							resession.load('last', { attach = false })
+						end),
+						button('q', '󰅚  Quit', ':qa<CR>'),
 					},
+					opts = { spacing = 1, position = 'center' },
 				},
 			}
 
 			local config = {
 				layout = {
 					theme.header,
-					theme.top_buttons,
-					theme.managers,
-					theme.sessions,
-					theme.mru_cwd,
-					theme.mru,
-					theme.bottom_buttons,
-					theme.footer,
+					{ type = 'padding', val = 2 },
+					theme.buttons,
 				},
 				opts = {
 					margin = 3,
-					redraw_on_resize = false,
+					redraw_on_resize = true,
 					setup = function()
 						vim.api.nvim_create_autocmd('DirChanged', {
 							pattern = '*',
@@ -204,7 +160,6 @@ return { -- TODO: Redo dashboard
 						and vim.o.filetype == ''
 
 					if fallback_on_empty then
-						-- require('neo-tree').close_all()
 						require('resession').detach()
 						vim.cmd('Alpha')
 						vim.cmd(event.buf .. 'bwipeout!')
