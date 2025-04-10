c20ec119656fc896c57c902b8d97f8a59cf6ff3d
Péter Csizmadia <csizipeti0723@gmail.com>
2025-04-10 23:00:34 +0200

    Set fileformat to unix
    Disable filenames from top

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,11 @@
-local function showFileInWin()
+--[[ local function showFileInWin()
 	local wins = vim.fn.filter(vim.api.nvim_tabpage_list_wins(0), function(_, v)
 		local cfg = vim.api.nvim_win_get_config(v)
 		return cfg.relative == '' or cfg.external
 	end)
 
 	return #wins > 1
-end
+end ]]
 
 ---@type LazyPluginSpec[]
 return {
@@ -17,7 +17,7 @@ return {
 			options = {
 				theme = 'catppuccin',
 				icons_enabled = true,
-				globalstatus = true,
+				-- globalstatus = true,
 			},
 			sections = {
 				lualine_a = {
@@ -37,20 +37,20 @@ return {
 			tabline = {
 				lualine_a = { { 'tabs', mode = 2, max_length = vim.o.columns } },
 			},
-			winbar = {
-				lualine_c = { { 'filename', cond = showFileInWin } },
-			},
+			--[[ winbar = { 
+        lualine_c = { { 'filename', cond = showFileInWin } }
+      },
 			inactive_winbar = {
-				lualine_c = { { 'filename', cond = showFileInWin } },
-			},
+        lualine_c = { { 'filename', cond = showFileInWin } }
+      }, ]]
 			extensions = {
 				'fzf',
 				'lazy',
+				'man',
 				'mason',
 				'nvim-dap-ui',
 				'oil',
 				'quickfix',
-				'toggleterm',
 				'trouble',
 			},
 		},
