44b03ddf98702db958b40996bd56d58fe66aa9df
Péter Csizmadia <csizipeti0723@gmail.com>
2025-04-10 23:01:35 +0200

    Change neovide cursor vfx mode

--- lua/csipa/neovide.lua
+++ lua/csipa/neovide.lua
@@ -1,5 +1,5 @@
 vim.g.neovide_theme = 'auto'
 vim.o.guifont = 'Iosevka Term:h15'
 vim.opt.linespace = 0
-vim.g.neovide_cursor_vfx_mode = 'railgun'
+vim.g.neovide_cursor_vfx_mode = 'pixiedust'
 vim.g.neovide_floating_corner_radius = 0.35
