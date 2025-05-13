17930356d411627d75355fed019422a7f165dd9e
Péter Csizmadia <csizipeti0723@gmail.com>
2025-05-13 02:48:33 +0200

    Send notifys if packages are not installed

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,10 +1,27 @@
+---@type string[]
+local ensure_installed = {
+	'eslint_d',
+	'hadolint',
+	'markdownlint',
+}
+
 ---@type LazyPluginSpec[]
 return {
 	{ -- Linting
 		'mfussenegger/nvim-lint',
 		event = { 'BufReadPre', 'BufNewFile' },
 		config = function()
-			local lint = require 'lint'
+			for _, linter in ipairs(ensure_installed) do
+				local installed = require('mason-registry').is_installed(linter)
+				if not installed then
+					vim.notify(
+						linter .. ' not installed! Run ":MasonInstall ' .. linter .. '"',
+						vim.log.levels.ERROR,
+						{ group = 'mason' }
+					)
+				end
+			end
+			local lint = require('lint')
 			lint.linters_by_ft = {
 				markdown = { 'markdownlint' },
 				docker = { 'hadolint' },
