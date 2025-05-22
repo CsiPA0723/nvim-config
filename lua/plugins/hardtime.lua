e09ed0dec47b7e2ed214afbee097364eacdcc6e4
Péter Csizmadia <csizipeti0723@gmail.com>
2025-05-22 02:17:32 +0200

    Add hardtime.nvim back

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,26 @@
+local notify_opts = { group = 'hardtime' }
+
+---@type LazyPluginSpec[]
+return {
+	{
+		'm4xshen/hardtime.nvim',
+		event = 'VeryLazy',
+		dependencies = { 'MunifTanjim/nui.nvim' },
+		opts = {
+			max_count = 5,
+			disable_mouse = false,
+			callback = function(text)
+				vim.notify(text, vim.log.levels.WARN, notify_opts)
+			end,
+		},
+		config = function(_, opts)
+			require('fidget.notification').set_config(notify_opts.group, {
+				name = 'Hardtime',
+				icon = ' ',
+				ttl = 6,
+				skip_history = true,
+			}, true)
+			require('hardtime').setup(opts)
+		end,
+	},
+}
