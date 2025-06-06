57bfee96788e789c8aac6ee0c16168127a1be342
Péter Csizmadia <csizipeti0723@gmail.com>
2025-06-06 05:55:21 +0200

    Add toggle for lualine

--- lua/utils/mpris.lua
+++ lua/utils/mpris.lua
@@ -9,8 +9,12 @@ local on_stderr = function(error, data, _)
    notify(error .. '\n' .. data, vim.log.levels.ERROR, { group = notif_group })
 end
 
+---@class CsiPA.mpris
+---@field private _set_enable fun(state: boolean): nil
+---@field private _get_enable fun(): boolean
 local M = {
    enabled = true,
+   can_fit = true,
    title = 'Unknown',
    artist = 'Unknown',
    track = 'Unknown - Unknown',
@@ -26,6 +30,18 @@ function M.setup()
       icon = ' ',
       ttl = 4,
    }, true)
+
+   Snacks.toggle
+      .new({
+         name = 'Media Player',
+         icon = ' ',
+         id = 'mpris',
+         which_key = true,
+         notify = false,
+         set = M._set_enable,
+         get = M._get_enable,
+      })
+      :map('<leader>m')
 
    ---@diagnostic disable-next-line: missing-fields
    M.job_metadata = Job:new({
@@ -83,11 +99,11 @@ function M.setup()
 
    vim.fn.timer_start(500, function(_)
       local max_size = math.floor(vim.o.columns / 3) - 1
-      if vim.o.columns < 90 and M.enabled then
-         M.enabled = false
+      if vim.o.columns < 90 and M.can_fit then
+         M.can_fit = false
          return
       elseif vim.o.columns >= 90 then
-         M.enabled = true
+         M.can_fit = true
       end
 
       if M.changed_song then
@@ -124,11 +140,19 @@ function M.setup()
 end
 
 function M.get_status()
-   return M.playing and M.enabled
+   return M.playing and M.can_fit and M.enabled
 end
 
 function M.lualine()
    return M.lualine_component
+end
+
+function M._set_enable(state)
+   M.enabled = state
+end
+
+function M._get_enable()
+   return M.enabled
 end
 
 return M
